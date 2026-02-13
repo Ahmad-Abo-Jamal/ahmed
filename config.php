daef9463dce037e643418432cf4be9b4f3c367fc
ahmadgamalali <32736840+ahmadgamalali@users.noreply.github.com>
2026-02-13 18:00:47 +0000

    Remove password hashing - revert to plain text passwords

--- config.php
+++ config.php
@@ -104,14 +104,6 @@ function sanitize($data) {
     return htmlspecialchars(trim($data), ENT_QUOTES, 'UTF-8');
 }
 
-function hashPassword($password) {
-    return password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
-}
-
-function verifyPassword($password, $hash) {
-    return password_verify($password, $hash);
-}
-
 function isAdmin() {
     return isset($_SESSION['admin_id']) && isset($_SESSION['admin_role']);
 }
